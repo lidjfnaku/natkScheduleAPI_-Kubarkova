e5e1424410ff5fb640d8d464d3c9c224983930d1
lidjfnaku <lidjfnaku@gmail.com>
2026-03-20 00:15:33 +0500

    Added lines to: Program.cs

--- CollegeSchedule/Program.cs
+++ CollegeSchedule/Program.cs
@@ -1,6 +1,18 @@
+using CollegeSchedule.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+DotNetEnv.Env.Load();
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+ $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+ $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+ $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+
+$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+builder.Services.AddDbContext<AppDbContext>(options =>
+ options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -17,6 +29,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseRouting();
+
 
 app.MapControllers();
 
